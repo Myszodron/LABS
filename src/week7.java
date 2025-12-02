508cdbd5757f7912505f25488147ab77eab2585d
Myszodron <darrak300@gmail.com>
2025-12-02 21:16:29 +0100

    Exercise 2(b)

--- src/week7.java
+++ src/week7.java
@@ -4,15 +4,16 @@ public class week7 {
     public static void Main(String[] args) {
 
         ArrayNames();
+        ArrayReverse();
 
     }
-
+    //
     private static long time(Runnable task) {
         long start = System.currentTimeMillis();
         task.run();
         return System.currentTimeMillis() - start;
     }
-
+    //
     private static void ArrayNames() {
 
         ArrayList<String> names = new ArrayList<>(
@@ -31,6 +32,32 @@ public class week7 {
 
         list.removeFirst();
         System.out.println("Updated list: " + list);
+    }
+    //
+    private static void ArrayReverse() {
+
+        int n = 8;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            list.add((int) (Math.random() * 10) + 1);
+
+        System.out.println("List of integers: " + list);
+
+        reverseList(list);
+
+        System.out.println("Reversed list: " + list);
+        System.out.println();
+    }
+
+    private static void reverseList(ArrayList<Integer> a) {
+        for (int l = 0, r = a.size() - 1; l < r;  l++, r--) {
+            int temp = a.get(l);
+            a.set(l, a.get(r));
+            a.set(r,temp);
+        }
+
+
     }
 
 }
