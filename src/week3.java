e3dfca09247f0e4d3cf34655c4016c91f21c36aa
Myszodron <darrak300@gmail.com>
2025-12-03 00:44:58 +0100

    everything prints out from main, single class

--- src/week3.java
+++ src/week3.java
@@ -1,43 +1,51 @@
-// Exercise 1
 public class week3 {
-    static void main(String[] args) {
+
+    public static void main(String[] args) {
+
+        coinFlip();
+        equationRoots();
+        constructTriangle();
+        calculateTax();
+        leapYear();
+        bmiCalculator();
+    }
+
+    // Exercise 1
+    private static void coinFlip () {
         System.out.println("Fair coin flip : " + (Math.random() < 0.5 ? "Heads" : "Tails"));
     }
-}
 
-// Exercise 2
-class w3 {
-    static void main(String[] args) {
+    // Exercise 2
+    private static void equationRoots() {
+
         int a = 2;
         int b = -8;
         int c = 5;
 
-
-        System.out.println(" Equation: " + a + "x^2" + b + "+x" + c + "=0");
+        System.out.print("Equation: " + a + "x^2" + b + "+x" + c + "=0 ");
 
         double delta = (Math.pow(b, 2) - 4 * a * c);
         double r1 = (-b + Math.sqrt(delta)) / (2 * a);
         double r2 = (-b - Math.sqrt(delta)) / (2 * a);
 
         if (delta < 0) {
-            System.out.println("Has no real roots");
+            System.out.println("has no real roots");
         } else if (delta == 0) {
-            System.out.println("Has one root:" + (-b / (2 * a)));
+            System.out.println("has one root:" + String.format("%.2f", -b / (2 * a)));
         } else {
-            System.out.println("Has two roots: " + r1 + " and " + r2);
+            System.out.println("has two roots: " + String.format("%.2f", r1) + " and " + String.format("%.2f", r2));
         }
     }
-}
-//Exercise 3
-class w4 {
-    static void main(String[] args) {
+
+    //Exercise 3
+    private static void constructTriangle() {
         int x = 1;
         int y = 5;
         int z = 6;
 
-        System.out.println("Sides: a=" +x + ", b=" + y + ", c=" + z);
+        System.out.println("Sides: a=" + x + ", b=" + y + ", c=" + z);
 
-        if (x + y > z && x + z >y && y + z > x) {
+        if (x + y > z && x + z > y && y + z > x) {
             double sp = (x + y + z) / 2.0;
             double area = Math.sqrt(sp * (sp - x) * (sp - y) * (sp - z));
 
@@ -45,12 +53,10 @@ class w4 {
         } else {
             System.out.println("It is not possible to construct a triangle with the provided data");
         }
-
     }
-}
-// Exercise 4
-class tax {
-    static void main(String[] args) {
+
+    // Exercise 4
+    private static void calculateTax() {
         double income = 75000, tax;
 
         if (income <= 2500) {
@@ -64,20 +70,18 @@ class tax {
 
         System.out.println("Tax = " + tax + "$");
     }
-}
-//Exercise 5
-class calendar {
-    static void main(String[] args) {
+
+    //Exercise 5
+    private static void leapYear() {
         int year = 2015;
         boolean leap = (year % 4 == 0 && year % 100 == 0) || (year % 400 == 0);
 
-        System.out.println(year);
-        System.out.println( leap ? "Is a leap year" : "Is not a leap year");
+        System.out.print(year);
+        System.out.println(leap ? " is a leap year" : " is not a leap year");
     }
-}
-// Exercise 6
-    class bmi {
-    static void main(String[] args) {
+
+    // Exercise 6
+    private static void bmiCalculator() {
         double mass = 82;
         double height = 1.82;
         System.out.println("Mass: " + mass + " kg");
@@ -96,4 +100,3 @@ class calendar {
         }
     }
 }
-
